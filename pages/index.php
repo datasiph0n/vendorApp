3fb75be0ad1903020ec2e2ecdf46f37db2a54db7
datasiph0n <admin@siph0n.net>
2015-09-06 18:14:40 +0100

    Create index.php

---
+++ pages/index.php
@@ -0,0 +1,64 @@
+<?php
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://v4-alpha.getbootstrap.com/favicon.ico">
+    <title>siph0n - Vendor Application</title>
+    <!-- Bootstrap core CSS -->
+    <link href="http://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="http://v4-alpha.getbootstrap.com/examples/cover/cover.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://v4-alpha.getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+            <div align="left"><img src="https://forum.siph0n.net/images/cleandark/logo.png"/></div>
+              <nav class="nav nav-masthead">
+                <a class="nav-link active" href="#">Home</a>
+              </nav>
+            </div>
+          </div>
+          <div class="inner cover">
+            <h1 class="cover-heading">Vendor Applicaton:</h1>
+            <p class="lead">This application is for <b><u>VENDORS</u></b> only.</p>
+            <p class="lead">Do you have a product that you wish to sell among our marketplace?
+            Whether it's dumps to kits;</br> Everything is welcome.</p>
+            Please be aware:</br> <u>If</u> you're successful with this application, you <u>will not</u> have a 'full' forum account<b>.</b></br>
+            You will be <u>limited</u> to the marketplace only.</br>
+            <u>All products</u> are verified by staff members to help prevent 'scams' or 'fake exploits'</br>
+            with an escrow service provided for all users. We cannot be accountable for any trade, or transaction made <u>without</u> following the correct marketplace procedures. Please use your brain.</br>
+            You must post your 'main' product within <u>3 days</u> of obtaining access;</br><u>Failure</u> to do so will result with your access being revoked.</br>
+            There is also a joining fee of 0.5btc - you can understand why.</br></br>
+            <p class="lead">
+              <a href="?apply" class="btn btn-lg btn-secondary">Learn more</a>
+            </p>
+          </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Credits to <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="http://v4-alpha.getbootstrap.com/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="http://v4-alpha.getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://v4-alpha.getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
